3f22d1e55ff18ff308c6c346a85c3052f7dd5b60
Shanea Du <935769692@qq.com>
2017-11-10 12:26:00 +0800

    complete...py generates songs_with_complete_genre.csv and songs_preprocess to be continued

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,15 +1,10 @@
 import os
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from matplotlib import pyplot as plt
-import lightgbm as lgb
 os.chdir('D:/Kaggle/reco_music/data')
 
 
 # print col with nulls and the exact number
 def check_columns_isnull(df):
-    if df.isnull().values.any() :
+    if df.isnull().values.any():
         columns_with_nan = df.columns[df.isnull().any()].tolist()
         print("columns with nulls:")
         for col in columns_with_nan:
